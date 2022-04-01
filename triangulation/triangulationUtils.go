be704efbb6984ab910d6a4266052c9ca0cc1373a
Sabatini Federico <Sabatinifedericoagustin@gmail.com>
2022-04-01 15:33:49 -0300

    basic structure for satellites

--- triangulation/triangulationUtils.go
+++ triangulation/triangulationUtils.go
@@ -1,5 +1,30 @@
 package triangulation
 
+type position struct {
+	X int32
+	Y int32
+}
+
+type satellite struct {
+	name string
+	pos  position
+}
+
+var sattellites = []satellite{
+	satellite{
+		name: "Kenobi",
+		pos:  position{-500, -200},
+	},
+	satellite{
+		name: "Skywalker",
+		pos:  position{100, -100},
+	},
+	satellite{
+		name: "Sato",
+		pos:  position{500, 100},
+	},
+}
+
 func GetLocation(distances ...float32) (x, y float32) {
 	return 20.0, 20.0
 }
