d328d483040cc07dd5b5a85df47dc6b5e27e3678
Sabatini Federico <Sabatinifedericoagustin@gmail.com>
2022-04-02 20:00:34 -0300

    binded controller to service

--- controller/satellite-controller.go
+++ controller/satellite-controller.go
@@ -1,19 +1,25 @@
 package controller
 
 import (
+	"encoding/json"
+
 	"github.com/SabatiniFederico/mercadolibre-quasar-challenge/entity"
+	"github.com/SabatiniFederico/mercadolibre-quasar-challenge/service"
 	"github.com/gin-gonic/gin"
 )
 
 func TopSecretMessage(ctx *gin.Context) {
-	var satellites entity.SatellitesInfo
+	var info entity.SatellitesInfo
 
-	err := ctx.ShouldBindJSON(&satellites)
+	err := ctx.ShouldBindJSON(&info)
 	if err == nil {
-		ctx.JSON(200, gin.H{
-			"position": "hola",
-			"message":  "este es un mensaje secreto ",
-		})
+		satellites := info.Satellites
+		x, y := service.GetLocation(satellites[0].Distance, satellites[1].Distance, satellites[2].Distance)
+		message := service.GetMessage(satellites[0].Message, satellites[1].Message, satellites[2].Message)
+
+		answer := entity.Solution{Position: entity.Point{X: float64(x), Y: float64(y)}, Message: message}
+		json.Marshal(answer)
+		ctx.JSON(200, answer)
 	} else {
 		ctx.JSON(404, gin.H{
 			"message": "se a producido un error",
