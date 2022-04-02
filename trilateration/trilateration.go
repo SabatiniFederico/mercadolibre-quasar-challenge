d2412d61a9264899e53e6296e75eda017f556425
Sabatini Federico <Sabatinifedericoagustin@gmail.com>
2022-04-02 11:55:58 -0300

    basic aproach for trilateration

--- trilateration/trilateration.go
+++ trilateration/trilateration.go
@@ -1,7 +1,15 @@
 package trilateration
 
-import "github.com/SabatiniFederico/mercadolibre-quasar-challenge/model"
+import (
+	"math"
 
-func solveTrilateration(p1 model.Position, p2 model.Position, p3 model.Position, distances []float32) model.Position {
-	return model.Position{50.0, 0.0}
+	"github.com/SabatiniFederico/mercadolibre-quasar-challenge/model"
+)
+
+func Solve2DTrilateration(p1 model.Point, p2 model.Point, p3 model.Point, distances []float64) model.Point {
+
+	coordinateX := (math.Pow(distances[0], 2) - math.Pow(distances[1], 2) + math.Pow(p2.X, 2)) / (2 * p2.X)
+	coordinateY := math.Sqrt(math.Pow(distances[0], 2) - math.Pow(coordinateX, 2))
+
+	return model.Point{X: coordinateX, Y: coordinateY}
 }
--- trilateration/trilateration_test.go
+++ trilateration/trilateration_test.go
@@ -1,21 +1,49 @@
 package trilateration
 
 import (
+	"fmt"
+	"math"
 	"testing"
 
 	"github.com/SabatiniFederico/mercadolibre-quasar-challenge/model"
 )
 
-func TestSolveTrilateration(t *testing.T) {
-	pos1 := model.Position{0, 0}
-	pos2 := model.Position{100, 0}
-	pos3 := model.Position{50, 30}
-	dists := []float32{50, 50, 30}
+func TestPreciseTrilateration(t *testing.T) {
+	pos1 := model.Point{X: 0, Y: 0}
+	pos2 := model.Point{X: 100, Y: 0}
+	pos3 := model.Point{X: 50, Y: 30}
+	dists := []float64{50, 50, 30}
 
-	actualPosition := solveTrilateration(pos1, pos2, pos3, dists)
-	expectedPosition := model.Position{50.0, 0.0}
+	actualPosition := Solve2DTrilateration(pos1, pos2, pos3, dists)
+	expectedPosition := model.Point{X: 50.0, Y: 0.0}
 
+	fmt.Printf("(%f %f)", actualPosition.X, actualPosition.Y)
 	if actualPosition != expectedPosition {
 		t.Errorf("The position expected was (%f, %f), but received (%f, %f)", actualPosition.X, actualPosition.Y, expectedPosition.X, expectedPosition.Y)
 	}
+}
+
+func TestNonPreciseTrilateration(t *testing.T) {
+	pos1 := model.Point{X: 0, Y: 0}
+	pos2 := model.Point{X: 100, Y: 0}
+	pos3 := model.Point{X: 50, Y: 30}
+	dists := []float64{100, 100, 55}
+
+	actualPosition := Solve2DTrilateration(pos1, pos2, pos3, dists)
+	expectedPosition := model.Point{X: 50.0, Y: 86.6025}
+
+	if isNonAccuratePosition(actualPosition, expectedPosition) {
+		t.Errorf("The position expected was (%f, %f), but received (%f, %f)", actualPosition.X, actualPosition.Y, expectedPosition.X, expectedPosition.Y)
+	}
+}
+
+func isNonAccuratePosition(actualPoint model.Point, expectedPoint model.Point) bool {
+	margin := 0.0001
+	if math.Abs(actualPoint.X-expectedPoint.X) > margin {
+		return true
+	}
+	if math.Abs(actualPoint.Y-expectedPoint.Y) > margin {
+		return true
+	}
+	return false
 }
