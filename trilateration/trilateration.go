f74fc45cd99baa181a585dc5a3a3665412e9cfc9
Sabatini Federico <Sabatinifedericoagustin@gmail.com>
2022-04-02 12:35:20 -0300

    generated noSolution error for impossible cases of trilateration

--- trilateration/trilateration.go
+++ trilateration/trilateration.go
@@ -1,15 +1,36 @@
 package trilateration
 
 import (
+	"errors"
 	"math"
 
 	"github.com/SabatiniFederico/mercadolibre-quasar-challenge/model"
 )
 
-func Solve2DTrilateration(p1 model.Point, p2 model.Point, p3 model.Point, distances []float64) model.Point {
+var ErrNoSolution = errors.New("there is no posible solution")
+
+const marginOfError = 0.0001
+
+func Solve2DTrilateration(p1 model.Point, p2 model.Point, p3 model.Point, distances []float64) (model.Point, error) {
 
 	coordinateX := (math.Pow(distances[0], 2) - math.Pow(distances[1], 2) + math.Pow(p2.X, 2)) / (2 * p2.X)
 	coordinateY := math.Sqrt(math.Pow(distances[0], 2) - math.Pow(coordinateX, 2))
 
-	return model.Point{X: coordinateX, Y: coordinateY}
+	if math.IsNaN(coordinateY) {
+		return model.Point{}, ErrNoSolution
+	}
+
+	//i have to check two possible solutions for Y
+	if isAccurateDistance(distances[2], math.Pow(p3.X-coordinateX, 2)+math.Pow(p3.Y-coordinateY, 2)) {
+		return model.Point{X: coordinateX, Y: coordinateY}, nil
+	}
+	if isAccurateDistance(distances[2], math.Pow(p3.X-coordinateX, 2)+math.Pow(p3.Y+coordinateY, 2)) {
+		return model.Point{X: coordinateX, Y: -coordinateY}, nil
+	}
+
+	return model.Point{}, ErrNoSolution
+}
+
+func isAccurateDistance(expectedDistance float64, actualDistance float64) bool {
+	return math.Abs(expectedDistance-actualDistance) >= marginOfError
 }
--- trilateration/trilateration_test.go
+++ trilateration/trilateration_test.go
@@ -14,9 +14,12 @@ func TestPreciseTrilateration(t *testing.T) {
 	pos3 := model.Point{X: 50, Y: 30}
 	dists := []float64{50, 50, 30}
 
-	actualPosition := Solve2DTrilateration(pos1, pos2, pos3, dists)
+	actualPosition, error := Solve2DTrilateration(pos1, pos2, pos3, dists)
 	expectedPosition := model.Point{X: 50.0, Y: 0.0}
 
+	if error != nil {
+		t.Errorf("There was no solution for expected position (%f, %f)", actualPosition.X, actualPosition.Y)
+	}
 	fmt.Printf("(%f %f)", actualPosition.X, actualPosition.Y)
 	if actualPosition != expectedPosition {
 		t.Errorf("The position expected was (%f, %f), but received (%f, %f)", actualPosition.X, actualPosition.Y, expectedPosition.X, expectedPosition.Y)
@@ -29,20 +32,36 @@ func TestNonPreciseTrilateration(t *testing.T) {
 	pos3 := model.Point{X: 50, Y: 30}
 	dists := []float64{100, 100, 55}
 
-	actualPosition := Solve2DTrilateration(pos1, pos2, pos3, dists)
+	actualPosition, error := Solve2DTrilateration(pos1, pos2, pos3, dists)
 	expectedPosition := model.Point{X: 50.0, Y: 86.6025}
+
+	if error != nil {
+		t.Errorf("There was no solution for expected position (%f, %f)", actualPosition.X, actualPosition.Y)
+	}
 
 	if isNonAccuratePosition(actualPosition, expectedPosition) {
 		t.Errorf("The position expected was (%f, %f), but received (%f, %f)", actualPosition.X, actualPosition.Y, expectedPosition.X, expectedPosition.Y)
 	}
 }
 
+func TestNoSolutionForTrilateration(t *testing.T) {
+	pos1 := model.Point{X: 0, Y: 0}
+	pos2 := model.Point{X: 100, Y: 0}
+	pos3 := model.Point{X: 50, Y: 30}
+	dists := []float64{10, 10, 10}
+
+	actualPosition, error := Solve2DTrilateration(pos1, pos2, pos3, dists)
+
+	if error == nil {
+		t.Errorf("There was a solution for imposible trilateration case (%f, %f)", actualPosition.X, actualPosition.Y)
+	}
+}
+
 func isNonAccuratePosition(actualPoint model.Point, expectedPoint model.Point) bool {
-	margin := 0.0001
-	if math.Abs(actualPoint.X-expectedPoint.X) > margin {
+	if math.Abs(actualPoint.X-expectedPoint.X) > marginOfError {
 		return true
 	}
-	if math.Abs(actualPoint.Y-expectedPoint.Y) > margin {
+	if math.Abs(actualPoint.Y-expectedPoint.Y) > marginOfError {
 		return true
 	}
 	return false
