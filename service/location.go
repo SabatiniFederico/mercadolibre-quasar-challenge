accde0d150adf74bf6a1f33460a19302b47e71e0
Sabatini Federico <Sabatinifedericoagustin@gmail.com>
2022-04-04 20:01:02 -0300

    refactored location service

--- service/location.go
+++ service/location.go
@@ -4,21 +4,20 @@ import (
 	"github.com/SabatiniFederico/mercadolibre-quasar-challenge/lib/trilateration"
 )
 
-func getPositions() map[string]trilateration.Point {
+func getPositions() []trilateration.Point {
 
-	positions := make(map[string]trilateration.Point)
-	positions["kenobi"] = trilateration.Point{X: -500.0, Y: -200.0}
-	positions["skywalker"] = trilateration.Point{X: 100.0, Y: -100.0}
-	positions["sato"] = trilateration.Point{X: 500.0, Y: 100.0}
+	kenobi := trilateration.Point{X: -500.0, Y: -200.0}
+	skywalker := trilateration.Point{X: 100.0, Y: -100.0}
+	sato := trilateration.Point{X: 500.0, Y: 100.0}
 
-	return positions
+	return []trilateration.Point{kenobi, skywalker, sato}
 }
 
 func GetLocation(distances ...float64) (x, y float64, err error) {
 
 	pos := getPositions()
 
-	solution, err := trilateration.Solve2DTrilateration(pos["kenobi"], pos["skywalker"], pos["sato"], distances)
+	solution, err := trilateration.Solve2DTrilateration(pos, distances)
 
 	return solution.X, solution.Y, err
 }
